2f8cfd4c7283569a791757f4f83a872038e10d00
Franco Fichtner <franco@lastsummer.de>
2014-11-09 10:57:12 +0100

    build: add FreeBSD-based base and kernel scripts
    This is so we can ditch freesbie and manual patching of the
    source tree.  Needs more cleanups and testing...

---
+++ build/base.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# Copyright (c) 2014 Franco Fichtner <franco@lastsummer.de>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+set -e
+
+# XXX move to common location
+SETSDIR=/tmp/sets
+SRCDIR=/usr/src
+CPUS=1
+
+mkdir -p ${SETSDIR}
+rm -rf ${SETSDIR}/base.txz
+
+# XXX wipe git repostory to avoid dirty build
+
+# XXX needs SRCCONF
+MAKEARGS="__MAKE_CONF="
+
+make -C${SRCDIR} -j${CPUS} buildworld ${MAKEARGS} NO_CLEAN=yes
+make -C${SRCDIR}/release obj ${MAKEARGS}
+make -C${SRCDIR}/release base.txz ${MAKEARGS}
+
+mv $(make -C${SRCDIR}/release -V .OBJDIR)/base.txz ${SETSDIR}/base.txz
