68978fd506d59eb105d287c4c30738f477d85fdd
Franco Fichtner <franco@lastsummer.de>
2014-11-15 11:40:41 +0100

    iso: bootstrap freesbie legacy foo for now

--- build/common.sh
+++ build/common.sh
@@ -105,6 +105,8 @@ setup_platform()
 	echo ">>> Setting up core in ${1}..."
 
 	# XXX horribe stuff follows...
+	cp ${TOOLSDIR}/freesbie2/extra/varmfs/varmfs.rc ${1}/etc/rc.d/varmfs
+	cp ${TOOLSDIR}/freesbie2/extra/etcmfs/etcmfs.rc ${1}/etc/rc.d/etcmfs
 	rm -rf ${1}/usr/sbin/pc-sysinstall
 	cd ${COREDIR} && cp -r * ${1}
 	mkdir ${1}/conf
