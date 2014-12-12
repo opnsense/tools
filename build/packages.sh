ba5b62e0443db6c9f8737a5328f1e8fe7f7e8819
Franco Fichtner <franco@opnsense.org>
2014-12-12 09:55:26 +0100

    build: add an easy-to-use pkg(8) repository set generator

---
+++ build/packages.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# Copyright (c) 2014 Franco Fichtner <franco@opnsense.org>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+set -e
+
+. ./common.sh
+
+mkdir -p ${SETSDIR}
+rm -f ${SETSDIR}/packages-*.txz
+
+setup_stage ${STAGEDIR}
+
+# rebuild expected FreeBSD structure
+mkdir -p ${STAGEDIR}/Latest
+mkdir -p ${STAGEDIR}/All
+
+# push packages to home location
+cp ${PACKAGESDIR}/* ${STAGEDIR}/All
+
+# needed bootstrap glue when no packages are on the system
+cd ${STAGEDIR}/Latest && ln -s ../pkg-*.txz pkg.txz
+
+# generate index files
+cd ${STAGEDIR} && pkg repo .
+
+echo -n ">>> Creating packages set... "
+
+tar -Jcf ${SETSDIR}/packages-`date '+%Y%m%d'`.txz *
+
+echo "done"
