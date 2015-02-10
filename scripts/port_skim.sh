5bb6b77f7ec3497542915a777a1a45109b4fcdb8
Franco Fichtner <franco@opnsense.org>
2015-02-10 08:10:41 +0100

    scripts: it's time to add the infamous `port_skim'
    A lot of time went into this gem to find changes in a
    vanilla FreeBSD tree and to pick them up via the config
    files provided by OPNsense and its current ports tree.
    
    A vanilla tree is expected under /usr/freebsd-ports as
    well as the standard layout of OPNsense.  If not the case,
    the config vars can be edited to adapt to your directory
    structure.
    
    A known weakness of this script is its thoughtlessness
    in case a port does an include-some-other-Makefile trick,
    e.g. vim-lite to vim or gettext-runtime to gettext.  The
    issue has been amended by marking these ports `sync' in
    our ports.conf in opnsense/tools.git.  A better fix would
    require a substantial rework of the ports infrastructure,
    something that I don't think will happen.
    
    Ports that are marked `sync' will also never update their
    dependencies, as this would e.g. in the case of vim pull
    in a lot of Xorg glue.
    
    In any case it works reliably now.  :)

---
+++ scripts/port_skim.sh
@@ -0,0 +1,117 @@
+#!/bin/sh
+
+# Copyright (c) 2015 Franco Fichtner <franco@opnsense.org>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+set -e
+
+PORT_LIST=/usr/tools/config/current/ports.conf
+FREEBSD=/usr/freebsd-ports
+OPNSENSE=/usr/ports
+
+while read PORT_NAME PORT_CAT PORT_OPT; do
+	if [ "$(echo ${PORT_NAME} | colrm 2)" = "#" ]; then
+		continue
+	fi
+
+	PORT=${PORT_CAT}/${PORT_NAME}
+
+	SOURCE=${OPNSENSE}
+	if [ ! -d ${OPNSENSE}/${PORT} ]; then
+		SOURCE=${FREEBSD}
+	fi
+
+	if [ "${PORT_OPT}" != "sync" ]; then
+		PORT_DEPS=$(echo $PORT; make -C ${SOURCE}/${PORT} \
+		    PORTSDIR=${SOURCE} all-depends-list | \
+		    awk -F"${SOURCE}/" '{print $2}')
+	else
+		PORT_DEPS=${PORT}
+	fi
+
+	for PORT in ${PORT_DEPS}; do
+		if [ ! -d ${FREEBSD}/${PORT} ]; then
+			continue;
+		fi
+
+		diff -rq ${OPNSENSE}/${PORT} ${FREEBSD}/${PORT} \
+		    > /dev/null && continue
+
+		NEW=1
+		for ITEM in ${PORTS_CHANGED}; do
+			if [ ${ITEM} = ${PORT} ]; then
+				NEW=0
+				break;
+			fi
+		done
+		if [ ${NEW} = 1 ]; then
+			PORTS_CHANGED="${PORTS_CHANGED} ${PORT}"
+		fi
+	done
+done < ${PORT_LIST}
+
+for PORT in $PORTS_CHANGED; do
+	(diff -ru ${OPNSENSE}/${PORT}/ ${FREEBSD}/${PORT}/ \
+	    2>/dev/null || true;) | less -r
+
+	echo -n "replace ${PORT} [y/N]: "
+	read yn
+	case $yn in
+	[yY] )
+		rm -fr ${OPNSENSE}/${PORT}
+		cp -a ${FREEBSD}/${PORT} ${OPNSENSE}/${PORT}
+		;;
+	esac
+done
+
+for ENTRY in ${FREEBSD}/*; do
+	ENTRY=${ENTRY##"${FREEBSD}/"}
+
+	case "$(echo ${ENTRY} | colrm 2)" in
+		[[:upper:]] ) ;;
+		* ) continue;;
+	esac
+
+	diff -rq ${OPNSENSE}/${ENTRY} ${FREEBSD}/${ENTRY} \
+	    > /dev/null || ENTRIES="${ENTRIES} ${ENTRY}"
+done
+
+if [ -n "${ENTRIES}" ]; then
+	(for ENTRY in ${ENTRIES}; do
+		diff -ru ${OPNSENSE}/${ENTRY}/ ${FREEBSD}/${ENTRY}/ \
+		    2>/dev/null || true;
+	done) | less -r
+
+	echo -n "replace Infrastructure [y/N]: "
+	read yn
+	case $yn in
+	[yY] )
+		for ENTRY in ${ENTRIES}; do
+			rm -r ${OPNSENSE}/${ENTRY}
+			cp -a ${FREEBSD}/${ENTRY} ${OPNSENSE}/
+		done
+		;;
+	esac
+fi
