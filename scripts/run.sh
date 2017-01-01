12a4432855ed5cd27b7c765c3f34643592ccb511
Franco Fichtner <franco@opnsense.org>
2017-01-01 14:47:38 +0100

    scripts: improve batch run script slightly
    Sometimes clean wasn't done on abort.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,3 @@
-make ${1} && make clean-obj
-make ${1} FLAVOUR=LibreSSL && make clean-obj
+# simple batch script for release builds
+make clean-obj && make ${1} FLAVOUR=OpenSSL && make clean-obj
+make clean-obj && make ${1} FLAVOUR=LibreSSL && make clean-obj
