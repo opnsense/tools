ed910d6c03fc87af46dc50f9f227208a42377e0e
Franco Fichtner <franco@opnsense.org>
2017-02-08 18:30:57 +0100

    scripts: make run.sh a little more flexible

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 # simple batch script for release builds
-make clean-obj && make ${1} FLAVOUR=OpenSSL && make clean-obj
-make clean-obj && make ${1} FLAVOUR=LibreSSL && make clean-obj
+make clean-obj && make ${*} FLAVOUR=OpenSSL && make clean-obj
+make clean-obj && make ${*} FLAVOUR=LibreSSL && make clean-obj
