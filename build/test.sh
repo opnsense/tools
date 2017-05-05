5ac89b1dddf6e96028cdc0bdc67429b99401f97a
Franco Fichtner <franco@opnsense.org>
2017-05-05 04:47:15 +0200

    build/test: moving to phpunit6

--- build/test.sh
+++ build/test.sh
@@ -41,7 +41,7 @@ setup_clone ${STAGEDIR} ${PLUGINSDIR}
 setup_chroot ${STAGEDIR}
 
 extract_packages ${STAGEDIR}
-install_packages ${STAGEDIR} pkg pear-PHP_CodeSniffer phpunit ${PRODUCT_PKGNAME}
+install_packages ${STAGEDIR} pkg pear-PHP_CodeSniffer phpunit6 ${PRODUCT_PKGNAME}
 # don't want to deinstall in case of testing...
 
 echo ">>> Running ${PLUGINSDIR} test suite..."
