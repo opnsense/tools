c792444bb9b3ff06991b564ae91200d2c68eaaf9
Franco Fichtner <franco@opnsense.org>
2017-08-14 17:07:41 +0200

    scripts: archive the nightly logs

--- scripts/nightly.sh
+++ scripts/nightly.sh
@@ -2,15 +2,26 @@
 
 # nightly build script
 
-(make clean-logs,obj 2>&1) > /dev/null
+VERSION=$(date '+%Y%m%d%H%M')
+LOGSDIR="/tmp/logs"
+
+for DIR in $(find ${LOGSDIR} -type d -depth 1); do
+	DIR=$(basename ${DIR})
+	tar -C ${LOGSDIR} -xzf ${DIR}.tgz ${DIR}
+	rm -r ${LOGSDIR}/${DIR}
+done
+
+(make clean-obj 2>&1) > /dev/null
+
+mkdir -p ${LOGSDIR}/${VERSION}
 
 for STAGE in update info base kernel distfiles; do
 	# we don't normally clean these stages
-	(time make ${STAGE} 2>&1) > /tmp/logs/${STAGE}.log
+	(time make ${STAGE} 2>&1) > ${LOGSDIR}/${VERSION}/${STAGE}.log
 done
 
 for FLAVOUR in OpenSSL LibreSSL; do
 	(make clean-packages FLAVOUR=${FLAVOUR} 2>&1) > /dev/null
 	(time make packages FLAVOUR=${FLAVOUR} 2>&1) \
-	    > /tmp/logs/packages-${FLAVOUR}.log
+	    > ${LOGSDIR}/${VERSION}/packages-${FLAVOUR}.log
 done
