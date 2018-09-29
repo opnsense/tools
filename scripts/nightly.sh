bf3ff171ece1cecfa78a9025a19776e172d5023b
Franco Fichtner <franco@opnsense.org>
2018-09-29 06:34:07 +0200

    scripts/nightly: postprocess build file for discarded packages

--- scripts/nightly.sh
+++ scripts/nightly.sh
@@ -2,7 +2,7 @@
 
 # nightly build script
 
-eval "$(make print-LOGSDIR,PRODUCT_ARCH,PRODUCT_VERSION,TARGETDIRPREFIX)"
+eval "$(make print-LOGSDIR,PRODUCT_ARCH,PRODUCT_VERSION,STAGEDIR,TARGETDIRPREFIX)"
 
 for RECYCLE in $(cd ${LOGSDIR}; find . -name "[0-9]*" -type f | \
     sort -r | tail -n +7); do
@@ -33,6 +33,35 @@ for STAGE in ports plugins core test; do
 	done
 
 	wait
+
+	if [ ${STAGE} != "ports" ]; then
+		continue
+	fi
+
+	# Find out which packages have been built but
+	# ultimately discarded.  We want to know this to
+	# put them on a list to keep them temporarily in
+	# order to speed up the build further:
+
+	DELETED="Deleting.files.for"
+	CREATED="Creating.package.for"
+	TMPFILE="${STAGEDIR}/.portpost"
+
+	for FLAVOUR in OpenSSL LibreSSL; do
+		LOG=${LOGSDIR}/${PRODUCT_VERSION}/${STAGE}-${FLAVOUR}.log
+
+		echo ">>> Discarded intermediate packages:" > ${TMPFILE}
+		grep ${DELETED} ${LOG} | awk '{ print $5 }' | \
+		    cut -f1 -d':' | sort >> ${TMPFILE}
+
+		for BUILT in $(grep ${CREATED} ${LOG} | \
+		    awk '{ print $4 }' | sort -u); do
+			grep -v "^${BUILT}$" ${TMPFILE} > ${TMPFILE}.next
+			mv ${TMPFILE}.next ${TMPFILE}
+		done
+
+		mv ${TMPFILE} ${LOG}.post
+	done
 done
 
 tar -C ${TARGETDIRPREFIX} -cJf \
