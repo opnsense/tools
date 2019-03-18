0db6bd79c62729a2e66ed838e8b72c7a4746dd5f
Neko Prog <nekoprog@gmail.com>
2019-03-18 11:38:19 +0800

    Fix style

--- build/arm.sh
+++ build/arm.sh
@@ -32,8 +32,7 @@ SELF=arm
 
 . ./common.sh
 
-if [ ${PRODUCT_ARCH} != armv6 -a \
-    ${PRODUCT_ARCH} != aarch64 ]; then
+if [ ${PRODUCT_ARCH} != armv6 -a ${PRODUCT_ARCH} != aarch64 ]; then
 	echo ">>> Cannot build arm image with arch ${PRODUCT_ARCH}"
 	exit 1
 fi
