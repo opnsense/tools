a0fbbe89223b0e9ad7be41c99e15ac3e3dcaffab
Neko Prog <nekoprog@gmail.com>
2019-03-26 06:56:30 +0800

    build: append PRODUCT_DEVICE to kernel

--- build/kernel.sh
+++ build/kernel.sh
@@ -31,7 +31,7 @@ SELF=kernel
 
 . ./common.sh
 
-KERNEL_SET=$(find ${SETSDIR} -name "kernel-*-${PRODUCT_ARCH}.txz")
+KERNEL_SET=$(find ${SETSDIR} -name "kernel-*-${PRODUCT_ARCH}-${PRODUCT_DEVICE}.txz")
 
 if [ -f "${KERNEL_SET}" -a -z "${1}" ]; then
 	echo ">>> Reusing kernel set: ${KERNEL_SET}"
