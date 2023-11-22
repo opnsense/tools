b6125cb05ec02bb5ca50d0c921b1970b5b477f2b
Franco Fichtner <franco@opnsense.org>
2023-11-22 15:50:55 +0100

    build/rename: need to retain PRODUCT_HASH here

--- build/rename.sh
+++ build/rename.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (c) 2016-2021 Franco Fichtner <franco@opnsense.org>
+# Copyright (c) 2016-2023 Franco Fichtner <franco@opnsense.org>
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -45,8 +45,9 @@ for ARG in ${@}; do
 		echo ">>> Repacking base set..."
 		BASESET=$(find_set base)
 		setup_set ${STAGEDIR}/work ${BASESET}
-		cp ${STAGEDIR}/work/usr/local/opnsense/version/base.obsolete \
+		cp ${STAGEDIR}/work/usr/local/opnsense/version/${ARG}.obsolete \
 		    ${STAGEDIR}/obsolete
+		PRODUCT_HASH=$(cat ${STAGEDIR}/work/usr/local/opnsense/version/${ARG}.hash)
 		setup_version ${STAGEDIR} ${STAGEDIR}/work ${ARG} ${STAGEDIR}/obsolete
 		rm ${BASESET}
 		generate_set ${STAGEDIR}/work ${BASESET}
@@ -80,6 +81,7 @@ for ARG in ${@}; do
 			KERNEL_NAME="kernel"
 		fi
 		setup_set ${STAGEDIR}/work ${KERNELSET}
+		PRODUCT_HASH=$(cat ${STAGEDIR}/work/usr/local/opnsense/version/${ARG}.hash)
 		setup_version ${STAGEDIR} ${STAGEDIR}/work ${ARG}
 		rm ${KERNELSET}
 		generate_set ${STAGEDIR}/work ${KERNELSET}
