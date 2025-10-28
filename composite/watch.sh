e2dfc431a0f1fc282de9bcde0e59ffe77936096d
Franco Fichtner <franco@opnsense.org>
2025-10-28 14:19:02 +0100

    composite/watch: either 2023 or 2025 but not 2024

--- composite/watch.sh
+++ composite/watch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (c) 2022-2024 Franco Fichtner <franco@opnsense.org>
+# Copyright (c) 2022-2023 Franco Fichtner <franco@opnsense.org>
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
