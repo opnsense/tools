43a519b0a9391672fda267fd6b78fb3d7dca76c9
Franco Fichtner <franco@opnsense.org>
2025-10-28 10:55:48 +0100

    composite: a little less of guessing
    Since we always run the script from the directory we can
    infer that util.sh is also there.

--- composite/factory.sh
+++ composite/factory.sh
@@ -25,7 +25,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 
-. ./../tools/composite/util.sh
+. $(dirname ${0})/util.sh
 
 load_make_vars PRODUCT_ARCH PRODUCT_CORE PRODUCT_ZFS SETSDIR
 
