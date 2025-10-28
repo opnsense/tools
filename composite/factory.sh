10d06be109530efe972128e1d2415fc1d23e0ff6
Franco Fichtner <franco@opnsense.org>
2025-10-28 10:34:34 +0100

    composite: utils bandaid for now

--- composite/factory.sh
+++ composite/factory.sh
@@ -25,7 +25,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 
-. ./composite/util.sh
+. ./../tools/composite/util.sh
 
 load_make_vars PRODUCT_ARCH PRODUCT_CORE PRODUCT_ZFS SETSDIR
 
